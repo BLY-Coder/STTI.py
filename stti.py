77a11d3d358a2fbb45dafabd991219b456ae9f4f
BLY <63339164+Bertranuco@users.noreply.github.com>
2020-12-25 11:02:49 +0100

    Update stti.py

--- stti.py
+++ stti.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-file= open("stti.txt",'r')
+file= open("file.txt",'r') #Chage this !!! 
 read_file= file.readlines()
 
 
 for i in b:
-	req = requests.get(""+i.replace("\n","")).text
+	req = requests.get("WEBSITE"+i.replace("\n","")).text #Change this !!!
 	aa = bs(req,"lxml")
 	bb = aa.find("str")
 	if bb != None and "<str>"+i.replace("\n","")+"<str>" != bb:
